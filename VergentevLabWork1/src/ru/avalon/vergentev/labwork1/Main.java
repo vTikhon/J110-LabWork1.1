aef90fd188f691d6f59b1a62000288700f16c862
vTikhon <101446665+vTikhon@users.noreply.github.com>
2022-03-16 10:50:44 +0300

    Add files via upload
    Lab1.1 version1

---
+++ VergentevLabWork1/src/ru/avalon/vergentev/labwork1/Main.java
@@ -0,0 +1,19 @@
+package ru.avalon.vergentev.labwork1;
+
+public class Main {
+    public static void main(String[] args) {
+        CountryParameters [] country = new CountryParameters [5];
+        country[0] = new CountryParameters("Russia", 17100, 146700000, "Moscow", 12600000);
+        country[1] = new CountryParameters("Finland", 338, 5500000, "Helsinki", 655000);
+        country[2] = new CountryParameters("France", 643.8, 67800000, "Paris", 2100000);
+        country[3] = new CountryParameters("Andorra", 0.467, 85400, "Andorra la Vella", 22600);
+        country[4] = new CountryParameters("Singapore", 0.725, 5700000);
+
+        country[0].print();
+
+        CountryParameters.printAll(country);
+
+
+
+    }
+}
