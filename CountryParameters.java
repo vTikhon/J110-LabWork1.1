a31f7d7092f0c550b0049d500708415dd353f531
Tikhon Vergentev <101446665+vTikhon@users.noreply.github.com>
2022-03-22 08:52:38 +0300

    Add files via upload

---
+++ CountryParameters.java
@@ -0,0 +1,112 @@
+package ru.avalon.vergentev.labwork1;
+
+public class CountryParameters {
+    private String nameOfCountry;
+    private double square;
+    private long populationOfCountry;
+    private String nameOfCity;
+    private long populationOfCity;
+
+
+    //Constructors block
+    public CountryParameters(String nameOfCountry, double square, long populationOfCountry, String nameOfCity, long populationOfCity) {
+        setNameOfCountry(nameOfCountry);
+        setSquare(square);
+        setPopulationOfCountry(populationOfCountry);
+        setNameOfCity(nameOfCity);
+        setPopulationOfCity(populationOfCity);
+    }
+
+    public CountryParameters(String nameOfCountry, double square, long populationOfCountry) {
+        this(nameOfCountry, square, populationOfCountry, null, 0);
+    }
+
+
+
+
+
+
+    //Methods block
+    public void print() {
+        System.out.println("Name of country: " + getNameOfCountry());
+        System.out.println("Square of country: " + getSquare() + " thousand sq. km.");
+        System.out.println("Population of country: " + getPopulationOfCountry() + " thousand people");
+        if (getNameOfCity() != null) {
+            System.out.println("Name of the capital: " + getNameOfCity());
+        }
+        else if (getPopulationOfCity() != 0) {
+            System.out.println("Population of the capital: " + getPopulationOfCity() + " people");
+        }
+        System.out.print('\n');
+    }
+
+    public static void printAll(CountryParameters [] country) {
+            country[0].print();
+            country[1].print();
+            country[2].print();
+            country[3].print();
+            country[4].print();
+        }
+
+
+
+
+
+
+
+    //Getters and Setters block
+    public String getNameOfCountry() {
+        return nameOfCountry;
+    }
+
+    public void setNameOfCountry(String nameOfCountry) {
+        if (nameOfCountry == null) {
+            throw new IllegalArgumentException("Name of countries must have the name");
+        }
+        this.nameOfCountry = nameOfCountry;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public void setSquare(double square) {
+        if (square < 0) {
+            throw new IllegalArgumentException("The square of countries can't be less than zero");
+        }
+        this.square = square;
+    }
+
+    public long getPopulationOfCountry() {
+        return populationOfCountry;
+    }
+
+    public void setPopulationOfCountry(long populationOfCountry) {
+        if (populationOfCountry < 0) {
+            throw new IllegalArgumentException("The population of countries can't be less than zero");
+        }
+        this.populationOfCountry = populationOfCountry;
+    }
+
+    public String getNameOfCity() {
+        return nameOfCity;
+    }
+
+    public void setNameOfCity(String nameOfCity) {
+        if (nameOfCountry == null) {
+            throw new IllegalArgumentException("Name of cities must have the name");
+        }
+        this.nameOfCity = nameOfCity;
+    }
+
+    public long getPopulationOfCity() {
+        return populationOfCity;
+    }
+
+    public void setPopulationOfCity(long populationOfCity) {
+        if (populationOfCity < 0) {
+            throw new IllegalArgumentException("The population of cities can't be less than zero");
+        }
+        this.populationOfCity = populationOfCity;
+    }
+}
